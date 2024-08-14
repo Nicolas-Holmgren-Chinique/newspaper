2563542833187e2999ea7e1fcd02c5e46061027b
Nicolas <cocolas753@gmail.com>
2024-08-13 22:59:41 -0700

    permissions and auth

--- articles/views.py
+++ articles/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
+
+# this import will prvent none logined in users from being able to access the create 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 
-# this import will prvent none logined in users from being able to access the create 
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Article
 # Create your views here.
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = (
         "title",
@@ -28,14 +30,22 @@ class ArticleUpdateView(UpdateView):
     )
     template_name = "article_update.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
     fields = (
@@ -44,6 +54,7 @@ class ArticleCreateView(CreateView):
         # "author",
     )
 
+# this code below assings the author of the article, to the user that is signed in and writing the code
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
